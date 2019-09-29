7a667819be21cdd190ecb4c767c2266dea623a64
Gabriel Mioni <gabriel.mioni@gmail.com>
2019-09-29 22:09:26 +0000

    Activate/Deactivate hooks

--- gm-frontend-gallery.php
+++ gm-frontend-gallery.php
@@ -6,3 +6,12 @@ Author: Gabriel Mioni
 Version: 0.1
 Author URI: gabrielmioni.com
 */
+function gmFrontendGalleryActivate() {
+    file_put_contents(dirname(__FILE__) . '/log', print_r('This is the water' . "\n", true), FILE_APPEND);
+}
+register_activation_hook( __FILE__, 'gmFrontendGalleryActivate');
+
+function gmFrontendGalleryDeactivate() {
+    file_put_contents(dirname(__FILE__) . '/log', print_r("And this is the well\n", true), FILE_APPEND);
+}
+register_deactivation_hook( __FILE__, 'gmFrontendGalleryDeactivate' );
