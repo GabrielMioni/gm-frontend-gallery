b66ef6b9300cd74576a5a39c0d770b02ebb0c8dc
Gabriel Mioni <gabriel.mioni@gmail.com>
2019-10-13 13:48:12 -0700

    Test to check if plugin submit route exists

---
+++ tests/Unit/test-api.php
@@ -0,0 +1,28 @@
+<?php
+
+
+class ApiTest extends WP_UnitTestCase
+{
+    protected $server;
+    protected $namespaced_route = 'gm-frontend-gallery/v1';
+
+    public function setUp()
+    {
+        parent::setUp();
+        /** @var WP_REST_Server $wp_rest_server */
+        global $wp_rest_server;
+        $this->server = $wp_rest_server = new \WP_REST_Server;
+        do_action('rest_api_init');
+    }
+
+    /** @test */
+    public function plugin_can_register_submit_route()
+    {
+        $gmFrontendGallery = new gmFrontendGallery();
+        $gmFrontendGallery::registerApiSubmitRoute();
+
+        $request = new WP_REST_Request('POST', '/gm-frontend-gallery/v1/submit');
+        $response = $this->server->dispatch($request);
+        $this->assertEquals(200, $response->get_status());
+    }
+}
