88047b24944478a7a00fec5977bf7206e1e05ccd
Gabriel Mioni <gabriel.mioni@gmail.com>
2019-10-13 15:26:49 -0700

    Test whether file/image submissions work

--- tests/Unit/test-api.php
+++ tests/Unit/test-api.php
@@ -25,9 +25,42 @@ class ApiTest extends WP_UnitTestCase
         $this->assertEquals(200, $response->get_status());
     }
 
+    /** @test */
+    public function file_data_can_be_submitted_to_api()
+    {
+        $path = $this->getPluginFilePath();
+
+        $testImagePath = $path . '/tests/images/conceited-ape.jpg';
+        //example: '/app/wp-content/plugins/gm-frontend-gallery/tests/images/conceited-ape.jpg';
+        $fileParams['file'] = [
+            'file' => file_get_contents($testImagePath),
+            'name' => 'conceited-ape.jpg',
+            'size' => filesize($testImagePath),
+            'tmp_name' => 'abc',
+        ];
+
+        $request = new WP_REST_Request('POST', $this->namespaced_route . '/submit');
+        $request->set_header('Content-Type', 'multipart/form-data');
+        $request->set_param('data', 'so much fun');
+        $request->set_file_params($fileParams);
+        $response = $this->server->dispatch($request);
+        $this->assertEquals(200, $response->get_status());
+    }
+
     protected function registerPluginSubmitRoute()
     {
         $gmFrontendGallery = new gmFrontendGallery();
         $gmFrontendGallery::registerApiSubmitRoute();
+    }
+
+    protected function getPluginFilePath()
+    {
+        $path = plugin_dir_path(__FILE__);
+        preg_match('/^(.*?)gm-frontend-gallery/', $path, $match);
+
+        if (empty($match)) {
+            return false;
+        }
+        return $match[0];
     }
 }
