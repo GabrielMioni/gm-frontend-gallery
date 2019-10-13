6a5b1dc6e8eedce603e8ca4003a3cfddf3507eb7
Gabriel Mioni <gabriel.mioni@gmail.com>
2019-10-13 21:01:50 +0000

    Moved route registration code out of test

--- tests/Unit/test-api.php
+++ tests/Unit/test-api.php
@@ -13,16 +13,21 @@ class ApiTest extends WP_UnitTestCase
         global $wp_rest_server;
         $this->server = $wp_rest_server = new \WP_REST_Server;
         do_action('rest_api_init');
+
+        $this->registerPluginSubmitRoute();
     }
 
     /** @test */
     public function plugin_can_register_submit_route()
     {
-        $gmFrontendGallery = new gmFrontendGallery();
-        $gmFrontendGallery::registerApiSubmitRoute();
-
         $request = new WP_REST_Request('POST', '/gm-frontend-gallery/v1/submit');
         $response = $this->server->dispatch($request);
         $this->assertEquals(200, $response->get_status());
+    }
+
+    protected function registerPluginSubmitRoute()
+    {
+        $gmFrontendGallery = new gmFrontendGallery();
+        $gmFrontendGallery::registerApiSubmitRoute();
     }
 }
