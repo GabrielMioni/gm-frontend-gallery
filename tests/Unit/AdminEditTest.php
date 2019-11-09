09dfe650ce328020e1d02e235385da04e087fbba
Gabriel Mioni <gabriel.mioni@gmail.com>
2019-11-09 13:31:33 -0800

    Added setup_for_trash_and_delete_tests() for reuse in other tests

--- tests/Unit/AdminEditTest.php
+++ tests/Unit/AdminEditTest.php
@@ -14,22 +14,34 @@ class AdminEditTest extends GalleryUnitTestCase
         parent::tearDown();
     }
 
-    /** @test */
-    public function gallery_posts_and_images_can_be_trashed()
+    protected function setup_for_trash_and_delete_tests()
     {
         $postResponse  = $this->createGalleryPostWithMultipleImages();
         $responseFiles = $postResponse['files'];
         $responseData  = $postResponse['response']->get_data();
-
-        $postId = $responseData['postID'];
-        $statusBeforeDelete = get_post_status($postId);
-
-        $this->assertNotFalse($statusBeforeDelete);
+        $postId        = $responseData['postID'];
 
         $userId = $this->factory()->user->create($this->default_user_values);
         $user = get_user_by( 'id', $userId);
         $user->set_role('administrator');
         wp_set_current_user($user->ID, $user->user_login);
+
+        return [
+            'postId' => $postId,
+            'postStatus' => get_post_status($postId),
+            'responseFiles' => $responseFiles,
+            'responseData' => $responseData
+        ];
+    }
+
+    /** @test */
+    public function gallery_posts_and_images_can_be_trashed()
+    {
+        $setupData = $this->setup_for_trash_and_delete_tests();
+        $postId = $setupData['postId'];
+        $statusBeforeDelete = $setupData['postStatus'];
+
+        $this->assertNotFalse($statusBeforeDelete);
 
         $request = new WP_REST_Request('DELETE', $this->namespaced_route . '/' . $postId);
         $request->set_header('Content-Type', 'application/json');
