27954cdb5f26fb2a0b7aebf2eea56cd12ab1f48a
Gabriel Mioni <gabriel.mioni@gmail.com>
2019-11-09 13:05:59 -0800

    Moved gallery_posts_and_images_can_be_trashed() to AdminEditTest.php

--- tests/Unit/SubmitTest.php
+++ tests/Unit/SubmitTest.php
@@ -118,29 +118,4 @@ class SubmitTest extends GalleryUnitTestCase
 
         $this->assertNotEquals('', $postAttachmentId);
     }
-
-    /** @test */
-    public function gallery_posts_and_images_can_be_trashed()
-    {
-        $postResponse  = $this->createGalleryPostWithMultipleImages();
-        $responseFiles = $postResponse['files'];
-        $responseData  = $postResponse['response']->get_data();
-
-        $postId = $responseData['postID'];
-        $statusBeforeDelete = get_post_status($postId);
-
-        $this->assertNotFalse($statusBeforeDelete);
-
-        $userId = $this->factory()->user->create($this->default_user_values);
-        $user = get_user_by( 'id', $userId);
-        $user->set_role('administrator');
-        wp_set_current_user($user->ID, $user->user_login);
-
-        $request = new WP_REST_Request('DELETE', $this->namespaced_route . '/' . $postId);
-        $request->set_header('Content-Type', 'application/json');
-        $response = $this->dispatchRequest($request);
-
-        $this->assertEquals(200, $response->get_status());
-        $this->assertEquals('trash', get_post_status($postId));
-    }
 }
