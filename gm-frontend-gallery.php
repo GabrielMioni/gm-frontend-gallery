dc2f012af3b2b506f159da2063ce22584b7349fd
Gabriel Mioni <gabriel.mioni@gmail.com>
2019-11-16 20:02:40 -0800

    Moved most methods into controllers. Condensed routes to registerApiRoutes()

--- gm-frontend-gallery.php
+++ gm-frontend-gallery.php
@@ -7,19 +7,25 @@ Version: 0.1
 Author URI: gabrielmioni.com
 */
 
-use GmFrontendGallery\Controller\BaseController;
+require_once(__DIR__ . '/Controllers/SubmitController.php');
+require_once(__DIR__ . '/Controllers/AdminController.php');
+require_once(__DIR__ . '/Controllers/GalleryController.php');
+
+use GmFrontendGallery\Controller\SubmitController;
+use GmFrontendGallery\Controller\AdminController;
+use GmFrontendGallery\Controller\GalleryController;
 
 if (!defined('WP_TEST_RUNNING')) {
     add_action( 'init', ['gmFrontendGallery', 'createPostType']);
-    add_action('rest_api_init', ['gmFrontendGallery', 'registerApiSubmitRoute']);
-    register_deactivation_hook(__FILE__, ['gmFrontendGallery', 'deactivate']);
+    add_action('rest_api_init', ['gmFrontendGallery', 'registerApiRoutes']);
+//    register_deactivation_hook(__FILE__, ['gmFrontendGallery', 'deactivate']);
 }
 
-class gmFrontendGalleryOne extends BaseController
+class gmFrontendGallery
 {
     public static function createPostType()
     {
-        $postType = self::$postType;
+        $postType = 'gallery';
 
         $labels = [
             'name' => 'Gallery Images',
@@ -34,5 +40,72 @@ class gmFrontendGalleryOne extends BaseController
         ];
 
         register_post_type($postType, $args);
+    }
+
+    public static function registerApiRoutes()
+    {
+        $submitController = new SubmitController();
+        $adminController = new AdminController();
+        $galleryController = new GalleryController();
+
+        register_rest_route( 'gm-frontend-gallery/v1', '/submit/', [
+            'methods' => 'POST',
+            'callback' => [$submitController, 'processGallerySubmission'],
+        ]);
+        register_rest_route( 'gm-frontend-gallery/v1', '(?P<postId>\d+)?(?:/(?P<permanent>\d+))?', [
+            'methods' => 'DELETE',
+            'callback' => [$adminController, 'deleteGalleryPostById'],
+            'args' => [
+                'postId',
+                'permanent'
+            ]
+        ]);
+        register_rest_route( 'gm-frontend-gallery/v1', '/image/(?P<postId>\d+)?(?:/(?P<attachmentId>\d+))?', [
+            'methods' => 'DELETE',
+            'callback' => [$adminController, 'deleteAttachmentById'],
+            'args' => [
+                'postId',
+                'attachmentId'
+            ]
+        ]);
+        register_rest_route( 'gm-frontend-gallery/v1', '/get(?:/(?P<orderBy>[a-zA-Z\s]+))?(?:/(?P<order>[a-zA-Z\s]+))?',
+            self::setGetRouteArray($galleryController, [
+                'orderBy',
+                'order'
+            ])
+        );
+
+        register_rest_route( 'gm-frontend-gallery/v1', '/get/(?P<orderBy>[a-zA-Z\s]+)/(?P<order>[a-zA-Z\s]+)',
+            self::setGetRouteArray($galleryController, [
+                'orderBy',
+                'order',
+            ])
+        );
+
+        register_rest_route( 'gm-frontend-gallery/v1', '/get/(?P<page>\d+)/(?P<results>\d+)(?:/(?P<orderBy>[a-zA-Z\s]+))?(?:/(?P<order>[a-zA-Z\s]+))?',
+            self::setGetRouteArray($galleryController, [
+                'page',
+                'results',
+                'orderBy',
+                'order'
+            ])
+        );
+
+        register_rest_route('gm-frontend-gallery/v1', '(?P<postId>\d+)?', [
+            'methods' => WP_REST_Server::READABLE,
+            'callback' => [$galleryController, 'retrieveGalleryPostSingle'],
+            'args' => [
+                'postId'
+            ]
+        ]);
+    }
+
+    protected static function setGetRouteArray(GalleryController $galleryController, array $args)
+    {
+        return [
+            'methods' => WP_REST_Server::READABLE,
+            'callback' => [$galleryController, 'retrieveGalleryPosts'],
+            'args' => $args
+        ];
     }
 }
