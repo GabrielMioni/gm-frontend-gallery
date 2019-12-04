2e208a811d9dd5a9d8e2d3647147b8a187028e57
Gabriel Mioni <gabriel.mioni@gmail.com>
2019-12-04 15:24:55 -0800

    Cleanup

--- Controllers/BaseController.php
+++ Controllers/BaseController.php
@@ -8,12 +8,6 @@ use WP_Post;
 
 abstract class BaseController
 {
-//    protected $postType = 'gallery';
-//    protected $postStatus = 'published';
-//    protected $galleryIncompleteCode = 'gallery_incomplete';
-//    protected $galleryAttachmentMetaKey = 'gm_gallery_attachment';
-//    protected $galleryPostOrderKey = 'gm_gallery_order';
-//    protected $galleryAttachmentOrderKey = 'gm_gallery_attachment_order';
 
     use \definitionsTrait;
 
