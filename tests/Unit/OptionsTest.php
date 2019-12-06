cff84ba84fe61c4c4040e01ea1b4d60fda72447e
Gabriel Mioni <gabriel.mioni@gmail.com>
2019-12-06 10:17:48 -0800

    Added plugin_options_can_be_updated()

--- tests/Unit/OptionsTest.php
+++ tests/Unit/OptionsTest.php
@@ -14,4 +14,26 @@ class OptionsTest extends GalleryUnitTestCase
         $this->assertTrue(isset($options['admin_must_approve']));
         $this->assertTrue(isset($options['allowed_mimes']));
     }
+
+    /** @test */
+    public function plugin_options_can_be_updated()
+    {
+        $user_required_key = 'user_required';
+        $this->createAdminUser();
+
+        $originalOptions = get_option($this->pluginOptionName);
+        $this->assertFalse($originalOptions[$user_required_key]);
+
+        $setNewOptions = $originalOptions;
+        $setNewOptions[$user_required_key] = true;
+
+        $request = new WP_REST_Request('POST', $this->routeNameSpace . '/options');
+        $request->set_param('updatedOptions', $setNewOptions);
+        $response = $this->dispatchRequest($request);
+
+        $newOptions = get_option($this->pluginOptionName);
+
+        $this->assertEquals(200, $response->get_status());
+        $this->assertEquals($originalOptions[$user_required_key], $newOptions[$user_required_key]);
+    }
 }
