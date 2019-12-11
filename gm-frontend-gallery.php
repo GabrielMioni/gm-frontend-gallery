de1e2ba444756f994b1d6da9c9c2669f1440e5ef
Gabriel Mioni <gabriel.mioni@gmail.com>
2019-12-10 20:10:44 -0800

    Added shortcode to create <div> where vue will mount

--- gm-frontend-gallery.php
+++ gm-frontend-gallery.php
@@ -15,6 +15,9 @@ $gmFrontendGallery = new gmFrontendGallery();
 register_activation_hook(__FILE__, function() use ($gmFrontendGallery) {
     $gmFrontendGallery->activate();
 });
-add_action('rest_api_init', function () use ($gmFrontendGallery) {
+add_action('rest_api_init', function() use ($gmFrontendGallery) {
     $gmFrontendGallery->registerApiRoutes();
+});
+add_shortcode('gm-gallery', function() use ($gmFrontendGallery) {
+    return $gmFrontendGallery->mountVueApp();
 });
