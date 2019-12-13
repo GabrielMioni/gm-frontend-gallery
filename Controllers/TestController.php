7b4dfdea0e74999256df7bd7a325837a18e1f46d
Gabriel Mioni <gabriel.mioni@gmail.com>
2019-12-13 12:24:51 -0800

    Dummy gallery posts should include attachments

--- Controllers/TestController.php
+++ Controllers/TestController.php
@@ -2,6 +2,8 @@
 
 namespace GmFrontendGallery\Controller;
 
+include_once( ABSPATH . 'wp-admin/includes/image.php' );
+
 class TestController extends BaseController
 {
     public function index()
@@ -25,6 +27,70 @@ class TestController extends BaseController
             $postIds[] = $id;
         }
 
+        foreach ($postIds as $postId) {
+            $this->processImageAttachments($postId);
+        }
+
         var_dump($postIds);
+    }
+
+    protected function processImageAttachments($postId)
+    {
+        $maxAttachments = 5;
+        $attachmentIds = [];
+        $order = 0;
+
+        $path = plugin_dir_path(__DIR__) . 'tests/images';
+        $files = preg_grep('/^([^.])/', scandir($path));
+
+        foreach ($files as $file) {
+
+            if (count($attachmentIds) > $maxAttachments) {
+                break;
+            }
+
+            $setFile = $path . '/' . $file;
+
+            $imageDatum = [
+                'file' => file_get_contents($setFile),
+                'name' => basename($setFile),
+                'size' => filesize($setFile),
+                'tmp_name' => 'abc',
+                'path' => $setFile,
+            ];
+
+            $attachmentIds[] = $this->createImageAttachment($imageDatum, $postId, $order);
+        }
+
+        return $attachmentIds;
+    }
+
+    protected function createImageAttachment(array $imageData, $postId, $attachmentOrder)
+    {
+        $uploadData = wp_upload_bits($imageData['name'], null, $imageData['file']);
+
+        $file_path = $uploadData['file'];
+        $file_name = basename($file_path);
+        $file_type = wp_check_filetype($file_name, null);
+        $attachment_title = sanitize_file_name(pathinfo($file_name, PATHINFO_FILENAME));
+        $wp_upload_dir = wp_upload_dir();
+
+        $post_info = [
+            'guid'           => $wp_upload_dir['url'] . '/' . $file_name,
+            'post_mime_type' => $file_type['type'],
+            'post_title'     => $attachment_title,
+            'post_content'   => '',
+            'post_status'    => 'inherit',
+        ];
+
+        $attach_id = wp_insert_attachment( $post_info, $file_path, $postId );
+        $attach_data = wp_generate_attachment_metadata( $attach_id, $file_path );
+
+        wp_update_attachment_metadata($attach_id, $attach_data);
+
+        add_post_meta($postId, $this->galleryAttachmentMetaKey, $attach_id, false);
+        add_post_meta($attach_id, $this->galleryAttachmentOrderKey, $attachmentOrder, true);
+
+        return $attach_id;
     }
 }
