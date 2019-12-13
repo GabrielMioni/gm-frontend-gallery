fb3ffd9c3e9d5823b69ae4dca0b0cc54a904889e
Gabriel Mioni <gabriel.mioni@gmail.com>
2019-12-13 11:02:35 -0800

    Get gallery post data when Vue is mounted

--- dist/app.js
+++ dist/app.js
@@ -86,6 +86,57 @@
 /************************************************************************/
 /******/ ({
 
+/***/ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./src/App.vue?vue&type=script&lang=js&":
+/*!******************************************************************************************************************************************!*\
+  !*** ./node_modules/babel-loader/lib??ref--4-0!./node_modules/vue-loader/lib??vue-loader-options!./src/App.vue?vue&type=script&lang=js& ***!
+  \******************************************************************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+//
+//
+//
+//
+/* harmony default export */ __webpack_exports__["default"] = ({
+  name: 'gmGallery',
+  data: function data() {
+    return {
+      galleryItems: ''
+    };
+  },
+  methods: {
+    getGalleryItems: function getGalleryItems() {
+      var self = this;
+      var xhr = new XMLHttpRequest(); // xhr.open('GET', 'https://wordpress-local.lndo.site/wp-json/gm-frontend-gallery/v1/get');
+
+      xhr.open('GET', '/wp-json/gm-frontend-gallery/v1/get/');
+
+      xhr.onload = function () {
+        console.log(xhr.status);
+        console.log(xhr.response);
+        console.log(xhr.responseText);
+        console.log(_typeof(xhr.responseText)); // self.galleryItems = JSON.parse(xhr.responseText);
+
+        var data = JSON.parse(xhr.responseText);
+        console.log(_typeof(data));
+        self.galleryItems = data; // self.galleryItems = data;
+      };
+
+      xhr.send();
+    }
+  },
+  mounted: function mounted() {
+    console.log('mounted');
+    this.getGalleryItems();
+  }
+});
+
+/***/ }),
+
 /***/ "./node_modules/process/browser.js":
 /*!*****************************************!*\
   !*** ./node_modules/process/browser.js ***!
@@ -569,7 +620,9 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("div", { attrs: { id: "HELLO" } }, [_vm._v("Hello. I am Vue!")])
+  return _c("div", { attrs: { id: "HELLO" } }, [
+    _vm._v("Hello. I am Vue! So nice to meet you.")
+  ])
 }
 var staticRenderFns = []
 render._withStripped = true
@@ -12691,15 +12744,17 @@ module.exports = g;
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _App_vue_vue_type_template_id_7ba5bd90___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./App.vue?vue&type=template&id=7ba5bd90& */ "./src/App.vue?vue&type=template&id=7ba5bd90&");
-/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+/* harmony import */ var _App_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./App.vue?vue&type=script&lang=js& */ "./src/App.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
 
-var script = {}
+
+
 
 
 /* normalize component */
 
-var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__["default"])(
-  script,
+var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
+  _App_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
   _App_vue_vue_type_template_id_7ba5bd90___WEBPACK_IMPORTED_MODULE_0__["render"],
   _App_vue_vue_type_template_id_7ba5bd90___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
   false,
@@ -12713,6 +12768,20 @@ var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_
 if (false) { var api; }
 component.options.__file = "src/App.vue"
 /* harmony default export */ __webpack_exports__["default"] = (component.exports);
+
+/***/ }),
+
+/***/ "./src/App.vue?vue&type=script&lang=js&":
+/*!**********************************************!*\
+  !*** ./src/App.vue?vue&type=script&lang=js& ***!
+  \**********************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_App_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../node_modules/babel-loader/lib??ref--4-0!../node_modules/vue-loader/lib??vue-loader-options!./App.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./src/App.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport */ /* harmony default export */ __webpack_exports__["default"] = (_node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_App_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
 
 /***/ }),
 
--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,29 @@
 <template>
-    <div id="HELLO">Hello. I am Vue!</div>
+    <div id="HELLO">Hello. I am Vue! So nice to meet you.</div>
 </template>
 
 <script>
   export default {
-
+    name: 'gmGallery',
+    data() {
+      return {
+        galleryItems: '',
+      }
+    },
+    methods: {
+      setGalleryItems() {
+        const self = this;
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', '/wp-json/gm-frontend-gallery/v1/get/');
+        xhr.onload = () => {
+          self.galleryItems = JSON.parse(xhr.responseText);
+        };
+        xhr.send();
+      }
+    },
+    mounted() {
+      console.log('mounted');
+      this.setGalleryItems();
+    }
   }
 </script>
