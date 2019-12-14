5243ca37aad6eb341450e756333f1aca05c6e8b3
Gabriel Mioni <gabriel.mioni@gmail.com>
2019-12-14 11:47:55 -0800

    Lightbox component

---
+++ src/gallery/js/components/GalleryLightBox.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="gm-gallery-light-box">
+        <div class="left"></div>
+        <div class="right"></div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'GalleryLightBox',
+    props: {
+      post: Object
+    }
+  }
+</script>
