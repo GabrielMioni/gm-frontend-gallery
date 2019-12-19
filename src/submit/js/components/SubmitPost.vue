f3a0c7e6959172e868d7e9addae4a2aa29357ba8
Gabriel Mioni <gabriel.mioni@gmail.com>
2019-12-19 11:02:18 -0800

    Individual gallery post submit

---
+++ src/submit/js/components/SubmitPost.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="gm-frontend-submit-post">
+        <div class="gm-frontend-submit-post-left">
+            <div class="gm-frontend-submit-post-upload">
+                This is the stone on which I will build my empire.
+            </div>
+        </div>
+        <div class="gm-frontend-submit-post-right">
+            <form>
+                <div class="gm-frontend-submit-form-group">
+                    <label :for="`gm-frontend-submit-title-${index}`">Title</label>
+                    <input type="text" name="title" :id="`gm-frontend-submit-title-${index}`">
+                </div>
+                <div class="gm-frontend-submit-form-group">
+                    <label :for="`gm-frontend-submit-author-${index}`">Author</label>
+                    <input type="text" name="author" :id="`gm-frontend-submit-author-${index}`">
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "SubmitPost",
+    props: {
+      post: Object,
+      index: Number,
+    },
+  }
+</script>
