f79a1664b32e4da5d5b9576cf62ee705498da975
Gabriel Mioni <gabriel.mioni@gmail.com>
2019-12-19 11:01:48 -0800

    Gallery submission 'form'

--- src/submit/js/App.vue
+++ src/submit/js/App.vue
@@ -1,9 +1,23 @@
 <template>
-    <div>This is the submit form</div>
+    <div id="gm-frontend-submit">
+        <template v-for="(post, index) in galleryPosts">
+            <submit-post
+                    :post="post"
+                    :index="index">
+            </submit-post>
+        </template>
+    </div>
 </template>
 
 <script>
+  import SubmitPost from "./components/SubmitPost";
   export default {
-    name: "gmGallerySubmit"
+    name: "gmGallerySubmit",
+    components: {SubmitPost},
+    data() {
+      return {
+        galleryPosts: [{},{},{}],
+      }
+    }
   }
 </script>
