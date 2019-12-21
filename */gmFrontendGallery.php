101d8936c40c3fb28d13cfe3193a48a7d12bb7ec
Gabriel Mioni <gabriel.mioni@gmail.com>
2019-12-20 17:48:15 -0800

    mountVueSubmit() includes data-nonce attribute with nonce value

--- */gmFrontendGallery.php
+++ */gmFrontendGallery.php
@@ -180,6 +180,7 @@ class gmFrontendGallery
     {
         wp_enqueue_script($this->scriptHandleSubmit);
         wp_enqueue_style($this->scriptHandleSubmit);
-        return '<div id="gm-frontend-submit"></div>';
+        $nonce = wp_create_nonce($this->gallerySubmitNonce);
+        return '<div id="gm-frontend-submit" data-nonce="'.$nonce.'"></div>';
     }
 }
