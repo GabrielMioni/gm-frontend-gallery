b747d2baa1db6bc29ce3d909581b34815a7fa995
Gabriel Mioni <gabriel.mioni@gmail.com>
2019-12-28 20:54:17 -0800

    Moved state/getters/muations/actions to postDataModule

--- src/submit/js/store/modules/postDataModule.js
+++ src/submit/js/store/modules/postDataModule.js
@@ -1,13 +1,57 @@
+import {defaultGalleryPostObject} from "../helpers";
+
 export const postDataModule = {
   namespaced: true,
+  state: {
+    galleryPosts: [defaultGalleryPostObject()],
+  },
   getters: {
-    getGalleryPostData (state, getters, rootState) {
+    getGalleryPosts: state => state.galleryPosts,
+    getGalleryPostData (state) {
       return (payload) => {
         if (typeof payload.deepKey !== 'undefined') {
-          return rootState.galleryPosts[payload.index][payload.type][payload.deepKey];
+          return state.galleryPosts[payload.index][payload.type][payload.deepKey];
         }
-        return rootState.galleryPosts[payload.index][payload.type]
+        return state.galleryPosts[payload.index][payload.type]
       }
     }
+  },
+  mutations: {
+    setPostContent(state, payload) {
+      state.galleryPosts[payload.index].content = payload.data;
+    },
+    setPostImageData(state, payload) {
+      state.galleryPosts[payload.index].imageUrl = payload.imageUrl;
+      state.galleryPosts[payload.index].file = payload.file;
+    },
+    setPostError(state, payload) {
+      state.galleryPosts[payload.index].errors[payload.type] = payload.error;
+    },
+    addPost(state) {
+      state.galleryPosts.push(defaultGalleryPostObject());
+    },
+    removePost(state, index) {
+      state.galleryPosts.splice(index, 1);
+      if (state.galleryPosts.length <= 0) {
+        state.galleryPosts.push(defaultGalleryPostObject());
+      }
+    }
+  },
+  actions: {
+    SET_POST_CONTENT(context, payload) {
+      context.commit('setPostContent', payload);
+    },
+    SET_POST_IMAGE_DATA(context, payload) {
+      context.commit('setPostImageData', payload)
+    },
+    SET_POST_ERROR(context, payload) {
+      context.commit('setPostError', payload);
+    },
+    ADD_POST(context) {
+      context.commit('addPost');
+    },
+    REMOVE_POST(context, index) {
+      context.commit('removePost', index);
+    },
   }
 };
--- src/submit/js/store/store.js
+++ src/submit/js/store/store.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import {defaultGalleryPostObject} from "./helpers";
-import {postDataModule} from "./modules/postDataModule";
+import { postDataModule } from "./modules/postDataModule";
 
 Vue.use(Vuex);
 
@@ -12,13 +11,11 @@ export const store = new Vuex.Store({
   state: {
     mainTitle: '',
     mainTitleError: '',
-    galleryPosts: [defaultGalleryPostObject()],
     postNonce: null,
   },
   getters: {
     getMainTitle: state => state.mainTitle,
     getPostNonce: state => state.postNonce,
-    getGalleryPosts: state => state.galleryPosts,
     getMainTitleError: state => state.mainTitleError,
   },
   mutations: {
@@ -28,28 +25,9 @@ export const store = new Vuex.Store({
     setPostNonce(state, nonce) {
       state.postNonce = nonce;
     },
-    setPostContent(state, payload) {
-      state.galleryPosts[payload.index].content = payload.data;
-    },
-    setPostImageData(state, payload) {
-      state.galleryPosts[payload.index].imageUrl = payload.imageUrl;
-      state.galleryPosts[payload.index].file = payload.file;
-    },
-    setPostError(state, payload) {
-      state.galleryPosts[payload.index].errors[payload.type] = payload.error;
-    },
     setMainTitleError(state, error) {
       state.mainTitleError = error;
     },
-    addPost(state) {
-      state.galleryPosts.push(defaultGalleryPostObject());
-    },
-    removePost(state, index) {
-      state.galleryPosts.splice(index, 1);
-      if (state.galleryPosts.length <= 0) {
-        state.galleryPosts.push(defaultGalleryPostObject());
-      }
-    }
   },
   actions: {
     SET_MAIN_TITLE(context, newTitle) {
@@ -58,23 +36,8 @@ export const store = new Vuex.Store({
     SET_POST_NONCE(context, postNonce) {
       context.commit('setPostNonce', postNonce);
     },
-    SET_POST_CONTENT(context, payload) {
-      context.commit('setPostContent', payload);
-    },
-    SET_POST_IMAGE_DATA(context, payload) {
-      context.commit('setPostImageData', payload)
-    },
-    SET_POST_ERROR(context, payload) {
-      context.commit('setPostError', payload);
-    },
     SET_MAIN_TITLE_ERROR(context, error) {
       context.commit('setMainTitleError', error);
-    },
-    ADD_POST(context) {
-      context.commit('addPost');
-    },
-    REMOVE_POST(context, index) {
-      context.commit('removePost', index);
     },
   },
 });
