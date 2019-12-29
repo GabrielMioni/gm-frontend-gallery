2eaecd10d41d6f104ec3b51411df6031a159fa56
Gabriel Mioni <gabriel.mioni@gmail.com>
2019-12-29 13:19:22 -0800

    Import/use PortalVue

--- src/submit/js/app.js
+++ src/submit/js/app.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { store } from './store/store';
+import PortalVue from 'portal-vue'
+
+Vue.use(PortalVue);
 
 new Vue({
   el: '#gm-frontend-submit',
