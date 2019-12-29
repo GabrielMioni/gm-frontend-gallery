8852648babc896ab1b8c6076ec8f57d1c4960290
Gabriel Mioni <gabriel.mioni@gmail.com>
2019-12-29 13:29:13 -0800

    skeleton for ConfirmationModal

---
+++ src/submit/js/components/ConfirmationModal.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="gm-frontend-confirmation">
+        <div class="gm-frontend-confirmation-modal">
+            <div>
+                <slot>Please confirm</slot>
+            </div>
+            <div>
+                <button>Cancel</button>
+                <button>Ok</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "ConfirmationModal"
+  }
+</script>
+
+<style scoped>
+
+</style>
