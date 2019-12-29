4ab8229598441b61a5e38ead1f3f470bf5f8d125
Gabriel Mioni <gabriel.mioni@gmail.com>
2019-12-28 21:11:01 -0800

    state/getters/mutators/actions related to main data moved to mainDataModule.js

--- src/submit/js/store/store.js
+++ src/submit/js/store/store.js
@@ -1,43 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import { postDataModule } from "./modules/postDataModule";
+import { mainDataModule } from "./modules/mainDataModule";
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
   modules: {
-    postData: postDataModule
-  },
-  state: {
-    mainTitle: '',
-    mainTitleError: '',
-    postNonce: null,
-  },
-  getters: {
-    getMainTitle: state => state.mainTitle,
-    getPostNonce: state => state.postNonce,
-    getMainTitleError: state => state.mainTitleError,
-  },
-  mutations: {
-    setMainTitle(state, newTitle) {
-      state.mainTitle = newTitle;
-    },
-    setPostNonce(state, nonce) {
-      state.postNonce = nonce;
-    },
-    setMainTitleError(state, error) {
-      state.mainTitleError = error;
-    },
-  },
-  actions: {
-    SET_MAIN_TITLE(context, newTitle) {
-      context.commit('setMainTitle', newTitle);
-    },
-    SET_POST_NONCE(context, postNonce) {
-      context.commit('setPostNonce', postNonce);
-    },
-    SET_MAIN_TITLE_ERROR(context, error) {
-      context.commit('setMainTitleError', error);
-    },
+    postData: postDataModule,
+    mainData: mainDataModule
   },
 });
