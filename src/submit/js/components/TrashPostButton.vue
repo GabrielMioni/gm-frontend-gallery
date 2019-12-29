7ca2ea6c63d5634e6e8d6ada71613f9cc62b0d76
Gabriel Mioni <gabriel.mioni@gmail.com>
2019-12-29 13:29:48 -0800

    Button component to trigger display of confirmation modal using portals

---
+++ src/submit/js/components/TrashPostButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <button @click="showModal = true">
+        x
+        <portal to="modals" v-if="showModal">
+            <confirmation-modal></confirmation-modal>
+        </portal>
+    </button>
+</template>
+
+<script>
+  import ConfirmationModal from "./ConfirmationModal";
+  export default {
+    name: "TrashPostButton",
+    components: {ConfirmationModal},
+    data() {
+      return {
+        showModal: false,
+      }
+    },
+  }
+</script>
