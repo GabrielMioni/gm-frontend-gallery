c28cc01999c95c7e9516e411c7988c236cde5aaf
Gabriel Mioni <gabriel.mioni@gmail.com>
2019-12-31 11:07:37 -0800

    LoadingButton component meant to be shared between Vue apps

---
+++ src/utilities/js/components/LoadingButton.vue
@@ -0,0 +1,32 @@
+<template>
+    <button class="gm-frontend-loading-button" @click.stop="submitAction()">
+        <span class="gm-frontend-loading-button-main">
+            <span v-bind:class="{ 'gm-frontend-submit-post-button-main--show' : !loading }">
+                <slot name="defaultText">
+                    Submit
+                </slot>
+            </span>
+            <span v-bind:class="{ 'gm-frontend-submit-post-button-main--show' : loading }">
+                <slot name="loadingText">
+                    Loading
+                </slot>
+            </span>
+        </span>
+    </button>
+</template>
+
+<script>
+  export default {
+    name: "LoadingButton",
+    props: {
+      loading: {
+        type: Boolean,
+        default: false,
+      },
+      submitAction: {
+        type: Function,
+        required: true,
+      }
+    }
+  }
+</script>
