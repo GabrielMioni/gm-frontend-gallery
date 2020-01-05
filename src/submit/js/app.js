446d9779ef069e7ab451382349cad6140c1b9047
Gabriel Mioni <gabriel.mioni@gmail.com>
2020-01-04 21:19:57 -0800

    Use Vuetify

--- src/submit/js/app.js
+++ src/submit/js/app.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuetify from 'vuetify'
 import { store } from './store/store';
 import PortalVue from 'portal-vue'
 
 Vue.use(PortalVue);
+Vue.use(Vuetify);
+
+const vuetify = new Vuetify();
 
 new Vue({
+  vuetify,
   el: '#gm-frontend-submit',
   store,
   render: h => h(App)
