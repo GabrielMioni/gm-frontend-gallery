10953a67b07e3d856cd40957ca9540c285c4097d
Gabriel Mioni <gabriel.mioni@gmail.com>
2020-02-19 16:23:20 -0800

    Added v-hover wrapper

--- src/gallery/js/components/GalleryPostDetailAttachedImage.vue
+++ src/gallery/js/components/GalleryPostDetailAttachedImage.vue
@@ -1,16 +1,18 @@
 <template>
-    <v-card
-            class="gm-frontend-gallery__detail__image-area__attached-images__image"
-            @click="updateSelectedImageIndex"
-            @keypress.enter="updateSelectedImageIndex"
-    >
-        <v-img
-                cover
-                height="100%"
-                width="100%"
-                :src="image['sized_images'].medium"
-        />
-    </v-card>
+    <v-hover
+            v-slot:default="{ hover }">
+        <v-card
+                :elevation="hover ? 12 : null"
+                class="gm-frontend-gallery__detail__image-area__attached-images__image"
+                @click="updateSelectedImageIndex"
+                @keypress.enter="updateSelectedImageIndex">
+            <v-img
+                    cover
+                    height="100%"
+                    width="100%"
+                    :src="image['sized_images'].medium"/>
+        </v-card>
+    </v-hover>
 </template>
 
 <script>
