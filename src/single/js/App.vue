d42a9bea45d9554604f816a21beacb45cf543792
Gabriel Mioni <gabriel.mioni@gmail.com>
2020-03-01 14:18:51 -0800

    Mount Vue, get the galleryPost WP post ID.

--- src/single/js/App.vue
+++ src/single/js/App.vue
@@ -1,13 +1,38 @@
 <template>
   <div class="gm-frontend-gallery__detail__single">
-    <gallery-post-detail></gallery-post-detail>
+    This is my Vue elm.
+    <gallery-post-detail v-if="false">
+    </gallery-post-detail>
   </div>
 </template>
 
 <script>
 import GalleryPostDetail from '@/gallery/js/components/GalleryPostDetail'
 export default {
-  name: 'App',
-  components: { GalleryPostDetail }
+  name: 'GalleryPostSingle',
+  components: { GalleryPostDetail },
+  data () {
+    return {
+      galleryPostId: false,
+      galleryPost: {}
+    }
+  },
+  methods: {
+    getSingleGalleryPost () {
+      // Go get that Gallery Post.
+    }
+  },
+  watch: {
+    galleryPostId (value) {
+      if (value === false) {
+        return
+      }
+      this.getSingleGalleryPost()
+    }
+  },
+  created () {
+    const mount = document.getElementById('gm-frontend-gallery-post-single')
+    this.galleryPostId = mount.dataset.id
+  }
 }
 </script>
