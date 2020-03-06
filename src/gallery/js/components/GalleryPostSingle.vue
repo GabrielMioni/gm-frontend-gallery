af3275150b374bab15cb859506c4c7e850a04b39
Gabriel Mioni <gabriel.mioni@gmail.com>
2020-03-05 18:33:13 -0800

    Component for rendering single gallery post pages. Uses the gallery Vuex store

---
+++ src/gallery/js/components/GalleryPostSingle.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="gm-frontend-gallery__detail__single">
+    This is my Vue elm.
+    <gallery-post-detail v-if="false">
+    </gallery-post-detail>
+  </div>
+</template>
+
+<script>
+import GalleryPostDetail from '@/gallery/js/components/GalleryPostDetail'
+import { mapActions } from 'vuex'
+export default {
+  name: 'GalleryPostSingle',
+  components: { GalleryPostDetail },
+  data () {
+    return {
+      galleryPostId: false,
+      galleryPost: {}
+    }
+  },
+  methods: {
+    ...mapActions({
+      SET_GALLERY_POSTS: 'galleryData/SET_GALLERY_POSTS'
+    })
+  },
+  created () {
+    const mount = document.getElementById('gm-frontend-gallery-post-single')
+    this.galleryPostId = mount.dataset.id
+  },
+  mounted () {
+    this.SET_GALLERY_POSTS({ time: 1000, postId: this.galleryPostId })
+  }
+}
+</script>
