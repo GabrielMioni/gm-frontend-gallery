42daca99a86aecd84390e625aa43fb81d092d6f0
Gabriel Mioni <gabriel.mioni@gmail.com>
2020-03-05 18:41:48 -0800

    Mount GalleryPostSingle

--- src/single/js/App.vue
+++ src/single/js/App.vue
@@ -1,38 +1,13 @@
 <template>
   <div class="gm-frontend-gallery__detail__single">
-    This is my Vue elm.
-    <gallery-post-detail v-if="false">
-    </gallery-post-detail>
+    <gallery-post-single></gallery-post-single>
   </div>
 </template>
 
 <script>
-import GalleryPostDetail from '@/gallery/js/components/GalleryPostDetail'
+import GalleryPostSingle from '@/gallery/js/components/GalleryPostSingle'
 export default {
-  name: 'GalleryPostSingle',
-  components: { GalleryPostDetail },
-  data () {
-    return {
-      galleryPostId: false,
-      galleryPost: {}
-    }
-  },
-  methods: {
-    getSingleGalleryPost () {
-      // Go get that Gallery Post.
-    }
-  },
-  watch: {
-    galleryPostId (value) {
-      if (value === false) {
-        return
-      }
-      this.getSingleGalleryPost()
-    }
-  },
-  created () {
-    const mount = document.getElementById('gm-frontend-gallery-post-single')
-    this.galleryPostId = mount.dataset.id
-  }
+  name: 'App',
+  components: { GalleryPostSingle }
 }
 </script>
